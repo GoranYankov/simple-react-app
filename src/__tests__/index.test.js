b2ac3f80491d3f480ec9ed591413db357f256eb9
GoranYanvko <goran415@abv.bg>
2019-10-09 18:37:25 +0300

    123

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -2,7 +2,7 @@ import { sum } from '../utils';
 
 describe('Name of the group', () => {
   it('should return 4', () => {
-    const result = sum(2, 3);
+    const result = sum(23, 3);
     expect(result).toEqual(5);
   });
 });
